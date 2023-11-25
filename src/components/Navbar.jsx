2be8132d65b3aac91d6535b3296490b753c0dbf9
physixkz <130267624+physixkz@users.noreply.github.com>
2023-11-24 22:10:39 -0800

    added code to Home.jsx and Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/experience"> Experience </Link>
+        <Link to="/aboutme"> About Me </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/resume"> Resume </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
